8a734ec0e78ea5da3de23c07709581714618cac2
Ryan <106076362+bry1ni@users.noreply.github.com>
2025-04-19 01:40:59 +0100

    fixes

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -3,4 +3,5 @@ import os
 API_BASE = os.getenv("API_BASE")
 EMAIL_AUTH = os.getenv("EMAIL_AUTH")
 PASSWORD_AUTH = os.getenv("PASSWORD_AUTH")
-SHOP_ID = os.getenv("SHOP_ID")
+SHOP_ID="1b06a2d6-9e4f-4afd-9aa6-aed77b67119e"
+PRODUCT_ID="f83b21c1-630d-4d03-8141-7282b911b27b"
--- src/tasks/executing/execute.py
+++ src/tasks/executing/execute.py
@@ -1,14 +1,14 @@
 from fastapi import HTTPException
 import requests
 from src.tasks.utils import get_token, auth_hdr
-from src.tasks import API_BASE, SHOP_ID
+from src.tasks import API_BASE, SHOP_ID, PRODUCT_ID
 
-def update_product(payload: dict, id: str, shop_id: str = SHOP_ID):
+def update_product(payload: dict, id_: str = PRODUCT_ID, shop_id: str = SHOP_ID):
     token = get_token()
-    endpoint = f"{API_BASE}/api/shops/{shop_id}/products/{id}/"
+    endpoint = f"{API_BASE}/api/shops/{shop_id}/products/{id_}/"
 
     try:
-        response = requests.post(endpoint, json=payload, headers=auth_hdr(token))
+        response = requests.put(endpoint, json=payload, headers=auth_hdr(token))
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
